7d0a91c89560d80754be98619c47ee7855de36e6
Aaron J Mitchell <amitchell1992@knights.ucf.edu>
2019-11-02 13:11:40 -0400

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,38 +6,38 @@ $(document).ready(function () {
     var targetNumber = Math.floor(Math.random() * 100) + 19;
     var userScore = 0;
 
-    
+    //Stone array inilialized to include the links to each of the stone images
     var stoneArray = ["https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/1/17/Soul_Stone_VFX.png/revision/latest?cb=20190427012633",
-    "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/9b/Reality_Stone_VFX.png/revision/latest?cb=20190427012609",
+        "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/9b/Reality_Stone_VFX.png/revision/latest?cb=20190427012609",
         "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/0a/Space_Stone_VFX.png/revision/latest?cb=20190427012702",
         "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/d/d7/Power_Stone_VFX.png/revision/latest?cb=20190427012543",
         "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/f/f0/Time_Stone_VFX.png/revision/latest?cb=20190427012724",
         "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/e/e4/Mind_Stone_VFX.png/revision/latest?cb=20190427012504"
     ];
 
-    for (var i = 0; i < stoneArray.length -1; i++) {
+    for (var i = 0; i < stoneArray.length - 1; i++) {
 
-        // For each iteration, we will create an imageCrystal
+        // For each iteration, we will create an imageStone
         var imageStone = $("<img>");
 
-        // First each crystal will be given the class ".crystal-image".
+        // First each stone will be given the class ".stones".
         // This will allow the CSS to take effect.
         imageStone.addClass("stones");
 
-        // Each imageCrystal will be given a src link to the crystal image
+        // Each imageStone will be given a src link to the stone
         imageStone.attr("src", stoneArray[i]);
 
-        // Each imageCrystal will be given a data attribute called data-crystalValue.
+        // Each imageStone will be given a data attribute called data-stoneValue.
         // This data attribute will be set equal to the array value.
         imageStone.attr("data-stoneValue", Math.floor(Math.random() * 12) + 1);
         imageStone.attr("height", "125px");
         imageStone.attr("width", "125px");
 
 
-        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+        // Lastly, each stone image (with all it classes and attributes) will get added to the page.
         $("#infinity-stones").append(imageStone);
     }
-
+    //This section is for creating a new stone that will be added to a different div
     var mindStone = $("<img>");
     mindStone.addClass("stone");
     mindStone.attr("src", stoneArray[5]);
@@ -48,7 +48,7 @@ $(document).ready(function () {
 
 
     updateStats();
-
+    //The onclick function for the stones. There are two classes since the stones are inside 2 divs at 2 different sizes
     $(".stone, .stones").on("click", function () {
 
         // Determining the crystal's value requires us to extract the value from the data attribute.
@@ -58,41 +58,38 @@ $(document).ready(function () {
 
         var stoneValue = ($(this).attr("data-stoneValue"));
         stoneValue = parseInt(stoneValue);
-        // We then add the crystalValue to the user's "counter" which is a global variable.
-        // Every click, from every crystal adds to the global counter.
+        // We then add the stoneValue to the user's "counter" which is a global variable.
+        // Every click, from every stone adds to the global counter.
         userScore += stoneValue;
 
         // All of the same game win-lose logic applies. So the rest remains unchanged.
-        alert("New score: " + userScore);
-
         if (userScore === targetNumber) {
-            alert("You win!");
             wins++;
             displayWin();
             newGame();
         } else if (userScore >= targetNumber) {
-            alert("You lose!!");
             losses++;
             displayLoss();
             newGame();
         }
         updateStats();
     });
-
+    //this functions updates the HTML after each stone clicl
     function updateStats() {
         $("#number-to-guess").text(targetNumber);
         $("#user-score").text(userScore);
         $("#wins").text(wins);
         $("#losses").text(losses);
     };
+    //to create a new game resetting the score and setting a new target number
     function newGame() {
         userScore = 0;
         targetNumber = Math.floor(Math.random() * 100) + 19;
         updateStats();
-        
-    }
 
-    function displayWin() {        
+    }
+    //Displays that the user has won and shows a gif
+    function displayWin() {
         $("#result-image").attr("src", "https://media.giphy.com/media/l3OAyWrV5CIjyiedmF/giphy.gif");
         $("#result-image").attr("height", "250px");
         $('#result').text("You Win!!");
@@ -100,11 +97,11 @@ $(document).ready(function () {
 
     }
 
+    //Displays that the user has lost and shows a gif
     function displayLoss() {
         $("#result-image").attr("src", "https://media.giphy.com/media/ie76dJeem4xBDcf83e/giphy.gif");
         $("#result-image").attr("height", "250px");
-       
         $('#result').text("You Lose!!");
-        
+
     }
 });
